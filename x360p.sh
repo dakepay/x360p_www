5e2cb6b049ccd448430b015e11b5aa4985a15ad4
xushuqing1018 <39081373+xushuqing1018@users.noreply.github.com>
2019-02-12 11:28:02 +0800

    Delete x360p.sh

--- x360p.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/php think queue:listen --queue SendWxTplMsg > /dev/null 2>&1 &
-/usr/bin/php think queue:listen --queue TransferMedia > /dev/null 2>&1 &
-/usr/bin/php think queue:listen --queue Base > /dev/null 2>&1 &
